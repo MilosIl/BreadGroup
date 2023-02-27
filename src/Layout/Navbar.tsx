7b7d070a116f7412de22f4ad436f6cb0fb2ac8df
Milos <milos.ilic20@hotmail.com>
2023-02-27 21:41:11 +0100

    feat: navigation skeleton

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,7 +1,21 @@
 import Link from 'next/link';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href='/'>Pocetna</Link>
+        </li>
+        <li>
+          <Link href='/signup'>Prijavi se</Link>
+        </li>
+        <li>
+          <Link href='/signup'>Uloguj se</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
