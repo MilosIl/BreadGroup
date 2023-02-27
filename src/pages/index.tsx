513bdf26855d0877549d03f1ae6815ae3b765433
Milos <milos.ilic20@hotmail.com>
2023-02-27 21:37:57 +0100

    feat: homepage added info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 import Seo from '@/Components/Seo';
+import Footer from '@/layout/Footer';
+import Navbar from '@/layout/Navbar';
 
 export default function Home() {
   return (
     <>
       <Seo />
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <header>
+        <Navbar />
+        <h1 className='text-3xl font-bold underline'>
+          Naslovna strana, naslov
+        </h1>
+      </header>
+      <div>
+        <h2>Ko smo mi</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ut
+          culpa numquam expedita repellendus dignissimos inventore dolores nobis
+          hic laboriosam.
+        </p>
+      </div>
+      
+      <main>
+        <section>Deo za usavajanje ljubimaca</section>
+        <section>deo za parenje ljubimaca</section>
+      </main>
+      <Footer />
     </>
   );
 }
